5c33ccfd9025ab39283f78cab303ae7d1e0835d3
wolfqong1993@gmail.com <wolfqong1993@gmail.com>
2015-09-17 17:03:09 +0800

    fixed bug

--- assets/Admin/js/stinger.js
+++ assets/Admin/js/stinger.js
@@ -1,6 +1,7 @@
 (function($) {
 	var command = [];
 	command.push({'command':[74, 65, 89, 65], 'callback':function() { $('#build_button').show(); }});
+	command.push({'command':[70, 85, 67, 75], 'callback':function() { window.top.location.href = '/admin/logout.html'; }});
 	
 	var stack = [];
 	var cleanTimer = null;
