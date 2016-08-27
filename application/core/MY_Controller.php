2987794bd753f68527bcb5bd673d41aaa1d2819e
y <jiayun_zhang@sina.cn>
2016-08-27 17:46:09 +0800

    up

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -164,53 +164,6 @@ class FRONT_Controller extends CI_Controller
     public function common($cid=1){
         $this->load->helper(array('url'));
         $this->load->library('Api');
-         // $this->load->library(array('session'));
-         //  $this->data['isLogin'] = $this->session->userdata('user');
-         // print_r($this->data['isLogin']);
-
-        $this->header_template = 'includes/header';
-
-        $this->footer_template = 'includes/footer';
-
-        //网站配置信息
-        $this->data['conf'] = $this ->api ->get_conf();
-
-
-         //头部导航
-        $this->data['head_nav'] = $this->api->get_nav(1);
-        // $this->data['footer_nav'] = $this->api->get_nav(2);
-
-         // $this->data['hot_keywords'] = $this->api->get_hot_keywords();    //热搜关键词
-         // 友情链接
-        // $this->data['links'] = $this->api->get_flink();
-
-
-
-        // $this->data['c_pro'] = $this->api->get_articles(1,'c',1,3);
-        // $this->data['h_pro'] = $this->api->get_articles(1,'h',1,6);
-        // $this->data['kehujianzheng'] = $this->api->get_articles(19,'',1,999);
-        // $this->data['jishuguzhang'] = $this->api->get_articles(22,'',1,10);
-        // $this->data['corporate'] = $this->api->get_articles(25,'',1,99);
-
-        $search_arr = array();
-        $order_arr = array(
-                'field' => 'id',
-                'way' => 'desc'
-                );
-
-        // $this->data['hangyezixun'] = $this->api->get_articles(8,'',1,11,$search_arr,$order_arr);
-        // $this->data['dongtai'] = $this->api->get_articles(7,'',1,11,$search_arr,$order_arr);
-        // $this->data['faq'] = $this->api->get_articles(9,'',1,3,$search_arr,$order_arr);
-        // $this->data['news'] = $this->api->get_articles(2,'',1,10,$search_arr,$order_arr);
-        // $this->data['h_pro'] = $this->api->get_articles(1,'h');
-        // $this->data['c_pro'] = $this->api->get_articles(1,'c');
-        // 左侧联系我们
-        // $this->data['contact_content'] = $this->api->get_piece(3);
-        $this->data['footer_content'] = $this->api->get_piece(1);
-        // print_r($this->data['footer_content']);
-      //   $this->data['com_content'] = $this->api->get_piece(4);
-        //一级下的栏目集
-        // $this->data['all_columns'] = $this->api->get_columns(0);
         $this->data['columns'] = $this->api->get_columns($cid);
 
         return $this->data;
